9b7f7c6f8705e392861298c0c7ea458d8c2ba33e
gugohh <oohhgug@gmail.com>
2021-12-28 23:06:11 +0100

    created the gippy app

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import { useEffect, useState } from 'react';
+import './App.css';
+
+
+
+function App() {
+
+  const [gif, setGif] = useState([])
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div className="App">
+      <section>
+
+      </section>
+    </div>
+  );
+}
+
+export default App;
