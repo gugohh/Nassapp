336a0e4956192b2af5e0a9212c4f44e812c929dc
gugohh <oohhgug@gmail.com>
2021-12-30 20:46:16 +0100

    work on search logic in component Home

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,24 +1,39 @@
 import React, { useState } from "react";
 import { Link } from "wouter";
 
+import './Home.css'
+
+
 const popGifs = ["nft-nfts", "money", "gold", "oil"]
 
 
 const Home = () => {
+
   const [keyword, setKeyword] = useState('')
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(keyword);
+  }
+
+  const handleChange = (e) => {
+    setKeyword(e.target.value)
+  }
   return (
-    <div>
+    <div className="home">
       <h1> Search Te best Gifs</h1>
-      <form action="">
-        <input type="text" value={keyword} />
+      <form onSubmit={handleSubmit}>
+        <input onChange={handleChange} type="text" value={keyword} />
       </form>
       <ul>
-        {popGifs.map((popGifs)=>(
-        <li key={popGifs}>
+        {popGifs.map((popGifs) => (
+          <li key={popGifs}>
 
-          <Link to={`/gif/${popGifs}`}>GIFs de {popGifs}</Link>
+            <Link to={`/gif/${popGifs}`}>GIFs de {popGifs}</Link>
 
-        </li>
+          </li>
         ))}
       </ul>
     </div>);
